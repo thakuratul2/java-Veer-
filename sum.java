aa9566dc33035c0d804c8ad519697d919df70f90
veer-pratapsingh <chaudharyreepa@gmail.com>
2022-09-03 20:56:00 +0530

    Day-2

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class sum
+{
+ public static void main(String arg[])
+ {
+ Scanner sc =new Scanner(System.in); 
+ int a,b,c;
+ System.out.println("Enter the value of Two Numbers:");
+ a=sc.nextInt();
+ b=sc.nextInt();
+ c=a+b;
+ System.out.println("Sum is "+c);
+ }
+
+}
